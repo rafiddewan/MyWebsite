8c65e3552b672669c16f76b17f7bf8048673c29d
rafiddewan <rafid.dewan145@gmail.com>
2020-04-20 21:58:16 -0400

    added read more and read less functionality

--- app.js
+++ app.js
@@ -1,3 +1,94 @@
+$(document).ready(function(){
+  $('#myVolunteerBtn').on('click', function() {
+    this.blur();
+    var $el = $(this),
+      textNode = this.lastChild;
+    $el.find('span').toggleClass('glyphicon-fire glyphicon-tint');
+    textNode.nodeValue = 'Read ' + ($el.hasClass('showRead') ? 'less' : 'more')
+    $el.toggleClass('showRead');
+  });
+  $('#myVolunteerBtn1').on('click',function() {
+    this.blur();
+    var $el = $(this),
+      textNode = this.lastChild;
+    $el.find('span').toggleClass('glyphicon-fire glyphicon-tint');
+    textNode.nodeValue = 'Read ' + ($el.hasClass('showRead') ? 'less' : 'more')
+    $el.toggleClass('showRead');
+  });
+  $('#myVolunteerBtn2').on('click',function() {
+    this.blur();
+    var $el = $(this),
+      textNode = this.lastChild;
+    $el.find('span').toggleClass('glyphicon-fire glyphicon-tint');
+    textNode.nodeValue = 'Read ' + ($el.hasClass('showRead') ? 'less' : 'more')
+    $el.toggleClass('showRead');
+  });
+  $('#myVolunteerBtn3').on('click',function() {
+    this.blur();
+    var $el = $(this),
+      textNode = this.lastChild;
+    $el.find('span').toggleClass('glyphicon-fire glyphicon-tint');
+    textNode.nodeValue = 'Read ' + ($el.hasClass('showRead') ? 'less' : 'more')
+    $el.toggleClass('showRead');
+  });
+  $('#myVolunteerBtn4').on('click',function() {
+    this.blur();
+    var $el = $(this),
+      textNode = this.lastChild;
+    $el.find('span').toggleClass('glyphicon-fire glyphicon-tint');
+    textNode.nodeValue = 'Read ' + ($el.hasClass('showRead') ? 'less' : 'more')
+    $el.toggleClass('showRead');
+  });
+  $('#myVolunteerBtn5').on('click',function() {
+    var $el = $(this),
+      textNode = this.lastChild;
+    $el.find('span').toggleClass('glyphicon-fire glyphicon-tint');
+    textNode.nodeValue = 'Read ' + ($el.hasClass('showRead') ? 'less' : 'more')
+    $el.toggleClass('showRead');
+  });
+  $('#myVolunteerBtn6').on('click',function() {
+    this.blur();
+    var $el = $(this),
+      textNode = this.lastChild;
+    $el.find('span').toggleClass('glyphicon-fire glyphicon-tint');
+    textNode.nodeValue = 'Read ' + ($el.hasClass('showRead') ? 'less' : 'more')
+    $el.toggleClass('showRead');
+  });
+  $('#myAwardBtn1').on('click',function() {
+    this.blur();
+    var $el = $(this),
+      textNode = this.lastChild;
+    $el.find('span').toggleClass('glyphicon-fire glyphicon-tint');
+    textNode.nodeValue = 'Read ' + ($el.hasClass('showRead') ? 'less' : 'more')
+    $el.toggleClass('showRead');
+  });
+  $('#myAwardBtn2').on('click',function() {
+    this.blur();
+    var $el = $(this),
+      textNode = this.lastChild;
+    $el.find('span').toggleClass('glyphicon-fire glyphicon-tint');
+    textNode.nodeValue = 'Read ' + ($el.hasClass('showRead') ? 'less' : 'more')
+    $el.toggleClass('showRead');
+  });
+  $('#myAwardBtn3').on('click',function() {
+    this.blur();
+    var $el = $(this),
+      textNode = this.lastChild;
+    $el.find('span').toggleClass('glyphicon-fire glyphicon-tint');
+    textNode.nodeValue = 'Read ' + ($el.hasClass('showRead') ? 'less' : 'more')
+    $el.toggleClass('showRead');
+  });
+  $('#myAwardBtn4').on('click',function() {
+    this.blur();
+    var $el = $(this),
+      textNode = this.lastChild;
+    $el.find('span').toggleClass('glyphicon-fire glyphicon-tint');
+    textNode.nodeValue = 'Read ' + ($el.hasClass('showRead') ? 'less' : 'more')
+    $el.toggleClass('showRead');
+  });
+});
+  
+
 $('body').scrollspy({ target: '#navbar-collapse-main' })
 $('[data-spy="scroll"]').each(function () {
     var $spy = $(this).scrollspy('refresh')
